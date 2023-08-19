bd9976396a758f7422b8dd5c3069193edbcfb8ff
Gift Amachree <111980187+jen67@users.noreply.github.com>
2023-08-20 00:18:14 +0100

    "Built with html, css and javaScript "

---
+++ calculator-app-main/README.md
@@ -0,0 +1,56 @@
+# Frontend Mentor - Calculator app solution
+
+This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Frontend Mentor - Calculator app solution](#frontend-mentor---calculator-app-solution)
+  - [Table of contents](#table-of-contents)
+  - [Overview](#overview)
+    - [The challenge](#the-challenge)
+    - [Screenshot](#screenshot)
+    - [Links](#links)
+  - [My process](#my-process)
+    - [Built with](#built-with)
+    - [Continued development](#continued-development)
+  - [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See the size of the elements adjust based on their device's screen size
+- Perform mathmatical operations like addition, subtraction, multiplication, and division
+- Adjust the color theme based on their preference
+- **Bonus**: Have their initial theme preference checked using `prefers-color-scheme` and have any additional changes saved in the browser
+
+### Screenshot
+
+![My Desktop view theme 1](./my-designs/desktop-view-1.png)
+
+### Links
+
+- Solution URL: [My solution URl](https://github.com/jen67/hosting/tree/main/calculator-app-main)
+- Live Site URL: [My live URL](https://jen67.github.io/hosting/calculator-app-main/index.html)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+### Continued development
+
+I intend to improve more on my JavaScript skills
+
+## Author
+
+- Frontend Mentor - [@jen67](https://www.frontendmentor.io/profile/jen67)
+- Twitter - [@JenniferAm96723](https://twitter.com/JenniferAm96723)
+- Linkedin - [@Gift Amachree](https://www.linkedin.com/in/gift-amachree-8a523623b/)
---
+++ calculator-app-main/index.js
@@ -0,0 +1,60 @@
+let switchSpan = document.querySelector(".switch span");
+let pElements = document.querySelectorAll(".numbers p");
+let switchContainer = document.querySelector(".switch");
+let body = document.querySelector("body");
+
+switchContainer.addEventListener("click", function(event) {
+    // Calculate the width of a single segment
+    let segmentWidth = switchContainer.offsetWidth / 3;
+
+    // Determine which segment was clicked
+    let clickedSegment = Math.floor((event.clientX - switchContainer.getBoundingClientRect().left) / segmentWidth);
+
+    // Calculate the new position for the span
+    let newPosition = pElements[clickedSegment].offsetLeft - switchSpan.parentElement.offsetLeft;
+    
+    // Set the position
+    switchSpan.style.transform = `translateX(${newPosition}px)`;
+
+    // Set the theme based on clicked segment
+    switch (clickedSegment) {
+        case 0:
+            body.className = "";  // default theme
+            break;
+        case 1:
+            body.className = "theme-2";
+            break;
+        case 2:
+            body.className = "theme-3";
+            break;
+    }
+});
+
+// Displaying clicked number or symbol in the <h2> display screen
+const display = document.getElementById('display');
+const gridContainer = document.querySelector('.grid-container');
+
+gridContainer.addEventListener('click', function(event) {
+    if (event.target !== gridContainer) {
+        const value = event.target.innerText;
+
+        switch (value) {
+            case 'DEL':
+                display.innerText = display.innerText.slice(0, -1);
+                break;
+            case 'RESET':
+                display.innerText = '';
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText.replace(/x/g, '*'));
+                } catch (error) {
+                    display.innerText = 'Error';
+                }
+                break;
+            default:
+                display.innerText += value;
+                break;
+        }
+    }
+});
