e8ad59aff7e6e981bae8ddb79c922b8836f060a8
Gift Amachree <111980187+jen67@users.noreply.github.com>
2024-01-05 19:20:49 +0100

    Update main.js

--- notifications-page-main/main.js
+++ notifications-page-main/main.js
@@ -1,17 +1,20 @@
 (function () {
-
   "use strict";
-  
+
   const toggleText = document.getElementById("toggle-text");
   const hiddenText = document.getElementById("hidden-text");
   const hideRedDots = document.querySelector(".header div");
   const redDots = document.querySelectorAll(".red-circle");
   const notifNumber = document.querySelector(".header h1 span");
+  const greybackground = document.querySelectorAll(".back_ground");
 
   //hide the red notifications
   hideRedDots.addEventListener("click", function () {
     redDots.forEach(function (dot) {
       dot.style.display = "none";
+    });
+    greybackground.forEach(function (background) {
+      background.style.backgroundColor = "transparent";
     });
     notifNumber.style.display = "none";
   });
