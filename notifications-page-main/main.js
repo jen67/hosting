44dc979ec2029fc87cf35e91ed19a3f969289873
Gift Amachree <111980187+jen67@users.noreply.github.com>
2024-01-05 18:58:43 +0100

    Update main.js

--- notifications-page-main/main.js
+++ notifications-page-main/main.js
@@ -1,11 +1,27 @@
-const toggleText = document.getElementById('toggle-text');
-const hiddenText = document.getElementById('hidden-text');
+(function () {
 
-// Initially, remove the "hidden" class to show the #hidden-text
-hiddenText.classList.remove('hidden');
+  "use strict";
+  
+  const toggleText = document.getElementById("toggle-text");
+  const hiddenText = document.getElementById("hidden-text");
+  const hideRedDots = document.querySelector(".header div");
+  const redDots = document.querySelectorAll(".red-circle");
+  const notifNumber = document.querySelector(".header h1 span");
 
-// Add a click event listener to the element
-toggleText.addEventListener('click', () => {
-  // Toggle the 'hidden' class to show/hide the element
-  hiddenText.classList.toggle('hidden');
-});
+  //hide the red notifications
+  hideRedDots.addEventListener("click", function () {
+    redDots.forEach(function (dot) {
+      dot.style.display = "none";
+    });
+    notifNumber.style.display = "none";
+  });
+
+  // Initially, remove the "hidden" class to show the #hidden-text
+  hiddenText.classList.remove("hidden");
+
+  // Add a click event listener to the element
+  toggleText.addEventListener("click", () => {
+    // Toggle the 'hidden' class to show/hide the element
+    hiddenText.classList.toggle("hidden");
+  });
+})();
